3913d9ca15216cbc164e21a65482a488537778e3
Josh <2315437w@student.gla.ac.uk>
2020-11-26 20:35:13 +0000

    Update main.py

--- flask_app/routes/main.py
+++ flask_app/routes/main.py
@@ -5,11 +5,11 @@ main = Blueprint('main', __name__)
 
 @main.route("/", methods = ['GET'])
 def home():
-    return render_template("/Templates/prototype.html")
+    return render_template("prototype.html")
 
 @main.route("/favicon.ico", methods = ['GET'])
 def fav():
-    return render_template("/Templates/prototype.html")
+    return render_template("prototype.html")
 
 
     
