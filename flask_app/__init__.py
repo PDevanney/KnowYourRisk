9d34e13f64308b769bbd56c22dd5b4febbcd4ef6
Josh <2315437w@student.gla.ac.uk>
2020-11-26 20:38:23 +0000

    Update main.py

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 from .routes.main import main
 
 def create_app():
-    app = Flask(__name__, template_folder="/Templates")
+    app = Flask(__name__, template_folder="/Templates/")
     app.register_blueprint(main)
     return app
